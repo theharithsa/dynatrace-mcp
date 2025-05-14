d247a3cf4fd920fa30d30f0db2c4f2a58ae944e3
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2025-05-14 10:02:12 +0200

    Added missing events read scope (fixes #14)

--- src/capabilities/get-logs-for-entity.ts
+++ src/capabilities/get-logs-for-entity.ts
@@ -1,38 +1,10 @@
 import { _OAuthHttpClient } from "@dynatrace-sdk/http-client";
-import { QueryExecutionClient } from '@dynatrace-sdk/client-query';
+import { executeDql } from "./execute-dql";
 
 
 
 export const getLogsForEntity = async (dtClient: _OAuthHttpClient, entityId: string) => {
-  const queryExecutionClient = new QueryExecutionClient(dtClient);
+  const dql = `fetch logs | filter dt.source_entity == "${entityId}"`;
 
-  const response = await queryExecutionClient.queryExecute({
-    body: {
-      query: `fetch logs | filter dt.source_entity == "${entityId}"`,
-    }
-  });
-
-  if (response.result) {
-    // return response result immediately
-    return response.result.records;
-  }
-  // else: We might have to poll
-  if (response.requestToken) {
-    // poll for the result
-    let pollResponse;
-    do {
-      // sleep for 2 seconds
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      pollResponse = await queryExecutionClient.queryPoll({
-        requestToken: response.requestToken,  
-      });
-      // done - let's return it
-      if (pollResponse.result) {
-        return pollResponse.result.records;
-      }
-    }
-    while (pollResponse.state === 'RUNNING' || pollResponse.state === 'NOT_STARTED');
-  }
-  // else: whatever happened - we have an error
-  return undefined;
+  return executeDql(dtClient, dql);
 };
